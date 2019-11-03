76b39ad7a094a23f5f4c0b1ac5a27e9abd0054f5
LincolnMA <machado.lincoln@gmail.com>
2019-11-03 15:08:18 -0300

    Inimigos spawnando

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from tkinter import *
+import random
 
 
 ##Functions
 def update_screen():
     screen.delete("all")
     render_fire()
+    #render_enemy_fire()
     render_player()
     render_enemies()
     screen.focus_set()
@@ -27,7 +29,19 @@ def render_player():
     if(to_down and y_player <= screensize_y): y_player = y_player + 1
     screen.create_image(x_player,y_player,image = airplane,anchor = S)
 def render_enemies():
-    print("Render enemies!")
+    out = -1
+    render_enemies.frame_count += 1
+    if render_enemies.frame_count >= delay_enemy.get():
+        render_enemies.frame_count = 0
+        x = random.randint(0,screensize_x)
+        y = 50
+        enemy_pos.append([x,y])
+    for i in range(0,len(enemy_pos)):
+        screen.create_image(enemy_pos[i][0],enemy_pos[i][1],image = heli,anchor = S)
+        enemy_pos[i][1]+=speed_enemy.get()
+        if(enemy_pos[i][1] >= screensize_y): out = i
+    if(out > -1): del(enemy_pos[out])
+        
 def to_left_on(x):
     global to_left
     to_left = True
@@ -59,11 +73,18 @@ def new_fire(x):
 window = Tk()
 airplane = PhotoImage(file = "navinha.gif")
 fire = PhotoImage(file = "fire.gif")
+heli = PhotoImage(file = "heli.gif")
 fire_pos = []
+enemy_pos = []
+render_enemies.frame_count = 0
 x_player = 400
 y_player = 600
 delay_update = IntVar()
 delay_update.set(500)
+delay_enemy = IntVar()
+delay_enemy.set(250)
+speed_enemy = IntVar()
+speed_enemy.set(1)
 to_left = False
 to_right = False
 to_up = False
@@ -83,6 +104,20 @@ sc1 = Scale(sidebar,from_ = 1,to = 1000,
             variable = delay_update,
             length = 150)
 sc1.grid(row = 1,column = 0)
+#Enemy Delay
+sc2 = Scale(sidebar,from_ = 10,to = 500,
+            label = "Enemy Delay(frames)",
+            orient = HORIZONTAL,
+            variable = delay_enemy,
+            length = 150)
+sc2.grid(row = 2,column = 0)
+#Enemy Speed
+sc3 = Scale(sidebar,from_ = 1,to = 10,
+            label = "Enemy speed(frames)",
+            orient = HORIZONTAL,
+            variable = speed_enemy,
+            length = 150)
+sc3.grid(row = 3,column = 0)
 ##Main Screen
 screen = Canvas(window,bg = "white",height = screensize_y,width = screensize_x)
 screen.grid(row = 0,column = 1)
